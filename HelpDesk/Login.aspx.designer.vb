e73cc5c385ddcab80765d2c14646fe3fa1b412d3
huamanhtuyen <huamanhtuyen@gmail.com>
2014-10-04 22:19:41 +0700

    chinh style

--- HelpDesk/Login.aspx.designer.vb
+++ HelpDesk/Login.aspx.designer.vb
@@ -47,7 +47,7 @@ Partial Public Class Login
     '''Auto-generated field.
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
-    Protected WithEvents LoginButton As Global.System.Web.UI.WebControls.Button
+    Protected WithEvents LoginButton As Global.Telerik.Web.UI.RadButton
 
     '''<summary>
     '''InvalidCredentialsMessage control.
