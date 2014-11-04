0449119c0f63c795b56f66ae02b613d5617903c5
huamanhtuyen <huamanhtuyen@gmail.com>
2014-11-04 14:14:17 +0700

    db may moi

--- TraCuuTKWeb/Settings.cs
+++ TraCuuTKWeb/Settings.cs
@@ -17,9 +17,14 @@ namespace t
         public static string sqlSTRINGADO_DONVI = "Data Source=10.224.33.12;Initial Catalog=t2c;User ID=ttdl;pwd=ttdl";
         public static string sqlSTRINGOLE = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\DesignDB.mdb;Persist Security Info=True;Jet OLEDB:Database Password=tuyenchim";
         public static string sqlSTRINGLITE = "Data Source= |DataDirectory|NACCS.VN.db;Version=3;New=False;Compress=True;";
-        public static string sqlSTRINGORACLE = "Data Source=//10.224.130.110:1521/orcl;User ID=VNACCSSHAREDINT;pwd=VNACCSSharedInt;Unicode=True;";
+        //public static string sqlSTRINGORACLE = "Data Source=//10.224.130.110:1521/orcl;User ID=VNACCSSHAREDINT;pwd=VNACCSSharedInt;Unicode=True;";
+        //public static string sqlSTRINGORACLE = "Data Source=//TC-APP-CCK.tongcuc.haiquan.vn:1521/ORCL;User ID=TRACUUTK;pwd=1;Unicode=True;";
 
         //public static string sqlSTRINGORACLE = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public static string sqlSTRINGORACLE =
+  "User Id=TRACUUTK;Password=1;Data Source=(DESCRIPTION=" +
+  "(ADDRESS=(PROTOCOL=TCP)(HOST=TC-APP-CCK.tongcuc.haiquan.vn)(PORT=1521))" +
+  "(CONNECT_DATA=(SID=ORCL)));";
 
         //Liên quan tới thư mục tạm, excel
         public static string TEMP_DIR = "";//Path.Combine(Server.MapPath("~"), "@temp");
